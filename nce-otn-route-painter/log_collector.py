f13ba67bb6459302aaff54d3b6c0769c1b472295
xiyu-deepin <ma138078@163.com>
2021-07-20 01:03:22 +0800

    [提交人]: xiyu [修改项]: 工具集合包占位

---
+++ nce-otn-route-painter/log_collector.py
@@ -0,0 +1,17 @@
+"""
+desc: 有关路由的关键日志信息提取器
+file: log_collector
+date: 2021-07-20
+"""
+
+import os.path
+import PyQt5
+
+
+class LogCollector:
+    def __init__(self): ...
+    def __str__(self): ...
+
+
+if __name__ == '__main__':
+    ex = LogCollector()
